584615dc1b7a7bb1aff7dbc75ee42ae9c4f8f216
NadifRahman <nadif.rah@gmail.com>
2024-06-19 16:28:38 -0400

    Add date to post schema

--- models/Post.js
+++ models/Post.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   msg: { type: String, maxLength: 280, required: true },
+  date_created: { type: Schema.Types.Date, required: true, default: Date.now },
 });
 
 module.exports = mongoose.model('Post', PostSchema);
